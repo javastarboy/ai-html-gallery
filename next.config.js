2280519954a062ce69b6e47aac575c0fa7a4d23c
liumingxing <594086597@qq.com>
2026-05-19 00:12:35 +0800

    fix: add image domain configuration for external images

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'oss.javastarboy.com',
+        pathname: '/agi/**',
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
